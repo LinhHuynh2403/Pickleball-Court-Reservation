1977b9ef1064eea09955dd672c5be005ef24c3f9
Linh Huynh <128510005+LinhHuynh2403@users.noreply.github.com>
2024-10-06 11:15:59 -0700

    Add files via upload

---
+++ pickleball_timetable.js
@@ -0,0 +1,26 @@
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+
+const hourPlay = [
+    {
+        time: "1 hour",
+        text: "You have choose to play on this court for 1 hour"
+    },
+    {
+        time: "2 hours",
+        text: "You have choose to play on this court for 2 hours"
+    },
+    {
+        time: "3 hours",
+        text: "You have choose to play on this court for 3 hours"
+    }
+];
+
+function update(hoursPLay) {
+    
+}
